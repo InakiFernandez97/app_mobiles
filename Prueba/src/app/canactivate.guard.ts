83d13921ab8a681b03afa5ca7d059ede65f7e02e
InakiFernandez <inakito2014@hotmail.com>
2024-10-04 16:57:44 -0300

    guard

--- Prueba/src/app/canactivate.guard.ts
+++ Prueba/src/app/canactivate.guard.ts
@@ -1,5 +1,19 @@
 import { CanActivateFn } from '@angular/router';
+import { Injectable } from '@angular/core';
+import { CanActivate, Router } from '@angular/router';
+import { AuthService } from './auth.service';
 
-export const canactivateGuard: CanActivateFn = (route, state) => {
+@Injectable({
+  providedIn: 'root',
+  })
+  export class canActivate implements CanActivate {
+  constructor(private authService: AuthService, private router: Router) {}
+  canActivate(): boolean {
+  if (this.authService.isLoggedIn()) {
   return true;
-};
+  } else {
+  this.router.navigate(['/inicio']);
+  return false;
+  }
+  }
+  }
