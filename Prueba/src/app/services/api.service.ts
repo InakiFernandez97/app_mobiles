9ed95816ac4917f6879f7b273fde0ce32783dd70
InakiFernandez <inakito2014@hotmail.com>
2024-10-11 10:40:03 -0300

    hola

---
+++ Prueba/src/app/services/api.service.ts
@@ -0,0 +1,17 @@
+import { PreloadAllModules } from '@angular/router';
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ApiService {
+  private url = 'https://jsonplaceholder.typicode.com';
+
+
+  constructor(private http: HttpClient) { }
+
+  getPost() {
+    return this.http.get(`${this.url}/posts`);
+  }
+}
